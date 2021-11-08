5634c9cd0f293ae28acae184ba941d7d84e3caa1
a-tiny-flame <atinyflame@gmail.com>
2021-11-08 13:34:55 -0600

    GRIDVIEW & BUILDER • Flutter Widget of the Day #07

---
+++ gridview_builder_07/android/app/src/main/kotlin/com/example/gridview_builder_07/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gridview_builder_07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gridview_builder_07/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: 10,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+          ),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50,
+                width: 50,
+                color: Colors.blue,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
