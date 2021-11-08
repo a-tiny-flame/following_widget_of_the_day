8c074915e522ef6f885f371d3f43ff3809be2f3a
a-tiny-flame <atinyflame@gmail.com>
2021-11-08 16:25:08 -0600

    SLIVER APP BAR • Flutter Widget of the Day #12

---
+++ silver_app_bar_12/lib/homepage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: CustomScrollView(
+        slivers: [
+          // sliver app bar
+          SliverAppBar(
+            backgroundColor: Colors.deepPurple,
+            leading: const Icon(Icons.menu),
+            expandedHeight: 300,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              background: Container(
+                color: Colors.pink,
+              ),
+              title: const Text('S L I V E R  A P P B A R'),
+            ),
+          ),
+
+          // sliver items
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 400,
+                  color: Colors.deepPurple[300],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 400,
+                  color: Colors.deepPurple[300],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 400,
+                  color: Colors.deepPurple[300],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
