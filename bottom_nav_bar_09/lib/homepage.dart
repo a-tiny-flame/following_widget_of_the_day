dc939a691c4c8732fba4c5214caa8796aa8d365a
a-tiny-flame <atinyflame@gmail.com>
2021-11-08 14:26:42 -0600

    BOTTOM NAV BAR • Flutter Widget of the Day #09

---
+++ bottom_nav_bar_09/lib/homepage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'pages/account.dart';
+import 'pages/home.dart';
+import 'pages/message.dart';
+import 'pages/settings.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final _pages = const <Widget>[
+    UserHome(),
+    UserMessage(),
+    UserSettings(),
+    UserAccount(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.deepPurple,
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
